fde4b65c043f38f61a6baaed8de5e68c0f851668
Anthony Le <le.anthony1993@gmail.com>
2023-11-06 15:29:50 +0000

    update script

--- aihao/audio_engine.py
+++ aihao/audio_engine.py
@@ -2,6 +2,11 @@ import pyttsx3
 
 def audio_engine():
     engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+    for voice in voices:
+        engine.setProperty('voice', voice.id)
+        engine.say('The quick brown fox jumped over the lazy dog.')
+    engine.runAndWait()
     return engine
 
 def assistantResponse(text):
@@ -13,8 +18,8 @@ def assistantResponse(text):
 
 
 def main():
-    assistantResponse("Hello Anthony")
-
+    # assistantResponse("Hello Anthony")
+    audio_engine()
 
 if __name__ == "__main__":
     main()
