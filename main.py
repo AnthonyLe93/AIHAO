09e205976e4dc66a168e48fb4707e933570ff5a6
Anthony Le <le.anthony1993@gmail.com>
2023-11-08 11:08:55 +0000

    adding conditional statements

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from audio_engine import AudioEngine
 from greeting import Greeting
-from speech_recognition import SpeechRecognition
+from speech_recognition_aihao import SpeechRecognition
 
 
 def main():
     audio_engine = AudioEngine()
-    audio_engine.assistant_response("Hello Anthony, what can i do for you?")
     audio_regcognizer = SpeechRecognition()
     audio_regcognizer.record_audio()
     greeting = Greeting()
@@ -18,6 +17,14 @@ def main():
                 responses = responses + greeting(text)
             except Exception as e:
                 responses = responses + ''
+        if 'date' in text:
+            get_date = get_date()
+            responses = responses + ' ' + get_date
+        if responses == '':
+            responses = responses + "I'm Sorry I Can't Do That Yet"
+
+        # Response
+        audio_engine.assistant_response(responses)
 
 
 if __name__ == "__main__":
