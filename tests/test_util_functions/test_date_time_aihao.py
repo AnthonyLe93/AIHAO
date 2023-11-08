24ea3723ba5102c9efec8c94106cb95c489e25a5
Anthony Le <le.anthony1993@gmail.com>
2023-11-08 16:45:17 +0000

    add unit test

---
+++ tests/test_util_functions/test_date_time_aihao.py
@@ -0,0 +1,19 @@
+import pytest
+from aihao.util_funtions.date_time_aihao import get_date
+import datetime
+import calendar
+from unittest.mock import Mock
+from freezegun import freeze_time
+
+# Define a test for the get_date function using pytest and freezegun
+@freeze_time("2023-11-07 14:30:00")
+def test_get_date():
+    # Call the function
+    result = get_date()
+
+    # Define the expected result with the specific day of the week (Tuesday)
+    expected_result = "Today is Tuesday, November the 7th"
+
+    # Assertions
+    assert result == expected_result
+
