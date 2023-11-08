85d5a30eca5135ac140ee96476161ad45093a032
Anthony Le <le.anthony1993@gmail.com>
2023-11-08 16:00:22 +0000

    add unit test

--- tests/test_audio_engine.py
+++ tests/test_audio_engine.py
@@ -1,6 +1,6 @@
 import pytest
 from aihao.audio_engine import AudioEngine
-
+from unittest.mock import Mock
 
 @pytest.mark.audio_engine
 class TestAudioEngine:
@@ -8,5 +8,27 @@ class TestAudioEngine:
     def audio_engine_obj(self):
         audio_engine = AudioEngine()
         return audio_engine
-    def test_audio_engine(self, audio_engine_obj):
-        pass
+    def test_get_voices(self, audio_engine_obj):
+        # Mock the 'voiceFemales' attribute to return a list of mocked voice objects
+        mocked_voice = Mock()
+        mocked_voice.name = "Mocked Voice"
+        mocked_voice.id = "mocked_id"
+        mocked_voice.age = 30
+        mocked_voice.gender = "VoiceGenderFemale"
+        mocked_voice.languages = ["English"]
+        audio_engine_obj.voiceFemales = [mocked_voice]
+        # Mock the 'engine' attribute methods to check if they are called
+        audio_engine_obj.engine.setProperty = Mock()
+        audio_engine_obj.engine.say = Mock()
+        audio_engine_obj.engine.runAndWait = Mock()
+
+        # Call the method under test
+        voices_id_list = audio_engine_obj.get_voices(text='Hello Anthony')
+
+        # Assertions
+        assert voices_id_list == ["mocked_id"]
+
+        # Check if the 'engine' attribute methods were called as expected
+        audio_engine_obj.engine.setProperty.assert_called_with('voice', 'mocked_id')
+        audio_engine_obj.engine.say.assert_called_with('Hello Anthony')
+        audio_engine_obj.engine.runAndWait.assert_called_once()
