8896bb5026378d1549c96248c808799e8b3aeccd
Anthony Le <le.anthony1993@gmail.com>
2023-11-08 11:09:46 +0000

    add datetime util

---
+++ aihao/util_funtions/date_time_aihao.py
@@ -0,0 +1,21 @@
+import datetime
+import calendar
+def get_date():
+    now = datetime.datetime.now()
+    my_date = datetime.datetime.today()
+    weekday = calendar.day_name[my_date.weekday()]
+    month_num = now.month
+    day_num = now.day
+
+    month_names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
+                   'November', 'December']
+
+    ordinal_numbers = ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th', '11th', '12th', '13th','14th', '15th', '16th', '17th', '18th', '19th', '20th', '21st', '22nd', '23rd', '24th', '25th', '26th', '27th', '28th', '29th', '30th', '31st']
+
+    return 'Today is ' + weekday + ', ' + month_names[month_num - 1] + ' the ' + ordinal_numbers[day_num - 1]
+
+
+def main():
+    get_date()
+if __name__ == "__main__":
+    main()
