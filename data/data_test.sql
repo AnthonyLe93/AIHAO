91ea1dcacb9b63ac4e16981c2b3d68cb86147b60
Anthony Le <le.anthony1993@gmail.com>
2023-11-13 14:38:37 +0000

    add test sql files

---
+++ data/data_test.sql
@@ -0,0 +1,4 @@
+INSERT INTO TEST_TABLE (username, password, balance)
+    VALUES ('test_user_1', 'test_user_1_password', 1000);
+INSERT INTO TEST_TABLE (username, password, balance)
+    VALUES ('test_user_2', 'test_user_2_password', 1234);
---
+++ data/schema_test.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS TEST_TABLE (
+    user_id BIGSERIAL PRIMARY KEY,
+    username TEXT,
+    password TEXT,
+    balance FLOAT
+);
