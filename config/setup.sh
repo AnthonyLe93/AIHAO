f242a12283841f040dfde695cc6b46e539cfd969
Anthony Le <le.anthony1993@gmail.com>
2023-11-13 15:21:50 +0000

    update setup files

--- config/setup.sh
+++ config/setup.sh
@@ -8,20 +8,8 @@ if [ $OS = "Darwin" ]; then
     if python3.10 -c "import sys; sys.exit(sys.version_info < (3, 10))"; then
         python3.10 -m venv ../aihao_venv
         source ../aihao_venv/bin/activate
+        brew install portaudio
         pip install -r ../requirements.txt
-    else
-        echo "AIHAO requires Python >=3.10, but yours is $(python3.10 --version)"
-        exit 1
-    fi
-elif [ $OS = "Linux" ]; then
-    echo "Detected Ubuntu"
-    if python3.10 -c "import sys; sys.exit(sys.version_info < (3, 10))"; then
-        cd /home/ubuntu/aihao-prod
-        sudo apt update -y
-        sudo apt-get install python3.10-venv
-        python3.10 -m venv aihao_venv
-        source ./aihao_venv/bin/activate
-        pip install -r requirements.txt
     else
         echo "AIHAO requires Python >=3.10, but yours is $(python3.10 --version)"
         exit 1
