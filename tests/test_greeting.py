fa86d0012353c6babd808ef32c00180e7dbe4d00
Anthony Le <le.anthony1993@gmail.com>
2023-11-13 10:47:43 +0000

    add unit test

--- tests/test_greeting.py
+++ tests/test_greeting.py
@@ -1,4 +1,7 @@
+import random
+
 import pytest
+from unittest.mock import patch
 from aihao.greeting import Greeting
 
 @pytest.mark.greeting
@@ -8,6 +11,28 @@ class TestGreeting:
         greeting = Greeting()
         return greeting
 
-    def test_audio_engine(self, greeting_obj):
-        pass
+    @patch('builtins.open', create=True)
+    def test_random_greetings(self, mock_open, greeting_obj):
+        # Mocking the open function
+        greeting_prompts = "greetings.txt"
+        mock_file = mock_open.return_value
+        mock_file.read.return_value = ("Hello\n"
+                                       "Hi\n"
+                                       "Hey\n")
+
+        # Mocking random.choice to always return "Hi"
+        with patch('random.choice', lambda x: "Hi"):
+            greeting_obj.random_greetings(greeting_prompts)
+
+        # Assert that open was called with the correct file path
+        mock_open.assert_called_once_with(greeting_prompts)
+
+        # Assert that read was called on the file object
+        mock_file.read.assert_called_once()
+
+        # Assert that the greeting_response is set to "Hi"
+        assert greeting_obj.greeting_response == "Hi"
+
+
+
 
