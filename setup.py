cfa6792dbfddc0d8efb6eb4eb63c91141c6edb0e
Anthony Le <le.anthony1993@gmail.com>
2023-11-14 12:58:48 +0000

    update setup

--- setup.py
+++ setup.py
@@ -4,6 +4,5 @@ setup(
     name='your_package_name',
     version='1.0.0',
     packages=find_packages(exclude=['config', 'notebooks']),  # Add 'aihao' as the parameter
-    package_dir={'': 'aihao'},  # Specify the 'src' directory
     # other setup parameters...
 )
