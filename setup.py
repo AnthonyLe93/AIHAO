7f7e4fac493bee91bdaebdd83374be3818e2f5d9
Anthony Le <le.anthony1993@gmail.com>
2023-11-14 19:09:03 +0000

    edit setup

--- setup.py
+++ setup.py
@@ -4,8 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='AIHAO',
-    version='0.0.1',
-    packages=find_packages(exclude=['config', 'notebooks']),  # Add 'aihao' as the parameter
-    package_dir={'aihao': 'aihao'},  # specify the 'aihao' directory as the root package directory
+    version='1.0.0',
+    packages=find_packages(include=['aihao', 'aihao.*'], exclude=['config', 'notebooks']),
 
 )
